21b3eda37b418f5a2162e1a1abdeb4a071ab79b8
Linda_Agnete <lalj52999@edu.ucl.dk>
2023-11-17 13:56:33 +0100

    SetUp database, first draft

---
+++ DatabaseScript/SetUp.sql
@@ -0,0 +1,27 @@
+CREATE DATABASE CLEAN
+USE CLEAN
+
+--Opret activities--
+
+CREATE TABLE Activities(
+	ID int IDENTITY(1,1) PRIMARY KEY,
+	Type varchar(255) NOT NULL,
+	PricePerUnit float NOT NULL,
+	TimePerUnit int NOT NULL,
+	Description varchar(MAX)
+	); 
+
+--Opret Clients--
+
+CREATE TABLE Clients(
+	ID int IDENTITY(1,1) PRIMARY KEY,
+	Name varchar (255) NOT NULL,
+	PhoneNumber varchar (11) NOT NULL,
+	Email varchar (255),
+	BillingAddress varchar (255) NOT NULL,
+	City varchar (255) NOT NULL,
+	ZipCode varchar (255) NOT NULL,
+	Country varchar (255) NOT NULL,
+	CVR int
+	);
+
